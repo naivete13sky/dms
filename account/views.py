26c7abb5a5b42a60be5879108854eeff22c43cbf
naivete13sky <chen320821@163.com>
2022-04-20 17:58:47 +0800

    注册1

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm
 
 def user_login(request):
     if request.method == "POST":
@@ -26,3 +26,18 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # 建立新数据对象但是不写入数据库
+            new_user = user_form.save(commit=False)
+            # 设置密码
+            new_user.set_password(user_form.cleaned_data['password'])
+            # 保存User对象
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
