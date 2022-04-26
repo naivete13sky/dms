2d0ee694f8b290cdb1a445357bd449574e53f8b2
naivete13sky <chen320821@163.com>
2022-04-26 18:34:49 +0800

    dashboard ok1

--- back_stage/urls.py
+++ back_stage/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'back_stage'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
 ]
--- back_stage/views.py
+++ back_stage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,r'../templates/index.html')
+
+def dashboard(request):
+    return render(request,r'dashboard.html')
