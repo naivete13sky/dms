147c42adef4005bdb208ae1bd03659e2a0c2fdec
naivete13sky <chen320821@163.com>
2022-08-17 15:22:13 +0800

    20220817  dashboard

--- back_stage/views.py
+++ back_stage/views.py
@@ -5,4 +5,7 @@ def index(request):
     return render(request,r'../templates/index.html')
 
 def dashboard(request):
-    return render(request,r'dashboard.html')
+    from job_manage.models import Job
+    job_counts=len(Job.objects.all())
+    print(job_counts)
+    return render(request,r'dashboard.html',{"job_counts":job_counts})
