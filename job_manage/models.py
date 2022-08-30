4381eb8b565b238611b8870213af7e8f12815d44
naivete13sky <chen320821@163.com>
2022-08-30 10:58:54 +0800

    20220830 Job增加了很多字段,梅自动化测试接口需要的2

--- job_manage/models.py
+++ job_manage/models.py
@@ -46,44 +46,63 @@ class JobManager(models.Manager):
         # return super(JobManager, self).get_queryset().all()
 
 class Job(models.Model):
+    file_usage_type = models.CharField(max_length=50,
+                                       choices=(('input_test', '导入测试'), ('customer_job', '客户资料'),
+                                                ('test', '测试'), ('else', '其它')), default='else', help_text='料号使用类型',
+                                       verbose_name="料号使用类型")
     # 当我们想设置最小长度的时候，但是在字段中没有的话，可以借助自定义验证器MinLengthValidator
     # FileField 为文件上传功能upload_to:对应的files创建的文件夹目录
-    file_compressed = models.FileField(upload_to='files', blank=True, null=True, verbose_name="整理过的原始文件")
-    file_compressed_org = models.FileField(upload_to='files', blank=True, null=True, verbose_name="原始文件")
-    file_odb = models.FileField(upload_to='files',blank=True, null=True,verbose_name="EP-ODB++")
-    file_org_type=models.CharField(max_length=10, choices=(('gerber274X', 'Gerber274-X'), ('gerber274D', 'Gerber274-D'),
-                                                           ('odb++', 'ODB++'), ('else', '其它')), default='else',verbose_name="原始料号类型")
-    file_odb_current = models.FileField(upload_to='files', blank=True, null=True, verbose_name="最新-EP-ODB++")
-    file_odb_g = models.FileField(upload_to='files', blank=True, null=True, verbose_name="G-ODB++")
-    vs_result_ep=models.CharField(max_length=10, choices=(('passed', '成功'), ('failed', '失败'), ('none', '未比对')), default='none',verbose_name="悦谱比图结果")
+    file_compressed = models.FileField(upload_to='files', blank=True, null=True,
+                                       help_text='整理过的原始文件.若是导入测试类型,则是rar压缩包,压缩包中只有一层文件夹.也可以是.tgz|.eps', verbose_name="整理过的原始文件")
+
+    job_name = models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=3)],
+                                help_text='料号名称,有可能有重复名字', verbose_name="料号名称")
+    file_odb_g = models.FileField(upload_to='files', blank=True, null=True, help_text='G软件转图的结果,导入测试类型需要填写此字段',
+                                  verbose_name="G-ODB++")
+    file_compressed_org = models.FileField(upload_to='files', blank=True, null=True, help_text='未整理的原始文件,rar压缩包',verbose_name="原始文件")
+    file_org_type = models.CharField(max_length=10,
+                                     choices=(('gerber274X', 'Gerber274-X'), ('gerber274D', 'Gerber274-D'),
+                                              ('odb++', 'ODB++'), ('eps', 'EPS'),('else', '其它')), default='else',
+                                     help_text='原始文件类型', verbose_name="原始文件类型")
+    from_object = models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=2)], null=True,
+                                   blank=True, help_text='料号从哪来的', verbose_name="料号来源")
+    status = models.CharField(max_length=10, choices=(('draft', '草稿'), ('published', '正式')), default='draft',
+                              help_text='草稿表示未经人工确认', )
+    # tags=TaggableManager()
+    # 声明这个manager也是基于我们自定义的模型类
+    tags = TaggableManager(through=TaggedWhatever)
+    remark = models.CharField(max_length=100, validators=[validators.MinLengthValidator(limit_value=0)], blank=True,
+                              null=True, help_text='料号的说明备注', verbose_name="备注")
+    file_odb = models.FileField(upload_to='files',blank=True, null=True,help_text='悦谱转图后的ODB++,确认过是对的',verbose_name="EP-ODB++")
+    file_odb_current = models.FileField(upload_to='files', blank=True, null=True, help_text='最新一次的悦谱转图结果,不需要手工录入,可在线自动生成',verbose_name="最新-EP-ODB++")
+
+    vs_result_ep=models.CharField(max_length=10, choices=(('passed', '成功'), ('failed', '失败'), ('none', '未比对')), default='none',help_text='导入测试管理员负责填写',verbose_name="悦谱比图结果")
     vs_result_g = models.CharField(max_length=10, choices=(('passed', '成功'), ('failed', '失败'), ('none', '未比对')),
-                                    default='none',verbose_name="G软件比图结果")
-    bug_info = models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=0)],blank=True, null=True,verbose_name="Bug信息")
-    job_name = models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=3)],verbose_name="料号名称")
+                                    default='none',help_text='导入测试管理员负责填写',verbose_name="G软件比图结果")
+    bug_info = models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=0)],blank=True, null=True,help_text='Bug信息',verbose_name="Bug信息")
+
     job_type = models.CharField(max_length=10, choices=(('common', '普通板'), ('hdi', 'HDI'), ('led', 'LED板'), ('car', '汽车板'), ('flex', '软板'),
                                                          ('rigid_flex', '软硬结合板'),('carrier', '载板'), ('else', '其它')), default='common',
-                                verbose_name="料号类型")
-    bool_layer_info=models.CharField(max_length=10, choices=(('true', 'true'), ('false', 'false')), default='false',null=True,blank=True,verbose_name="是否有层别信息")
-    remark = models.CharField(max_length=100, validators=[validators.MinLengthValidator(limit_value=0)],verbose_name="备注",blank=True,null=True)
+                                help_text='料号的类型',verbose_name="料号类型")
+    bool_layer_info=models.CharField(max_length=10, choices=(('true', 'true'), ('false', 'false')), default='false',null=True,blank=True,help_text='不需要人工填写,系统用',verbose_name="是否有层别信息")
 
-    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_manage_jobs',null=True,blank=True,verbose_name="负责人")
-    from_object=models.CharField(max_length=20, validators=[validators.MinLengthValidator(limit_value=2)],null=True,blank=True,verbose_name="料号来源")
+
+    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_manage_jobs',null=True,blank=True,help_text='料号上传人',verbose_name="负责人")
+
     publish = models.DateTimeField(default=timezone.now,null=True,blank=True,verbose_name='发布时间')
     create_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
     updated = models.DateTimeField(auto_now=True,verbose_name='更新时间')
-    STATUS_CHOICES = (('draft', '草稿'), ('published', '正式'))
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
     vs_time_ep = models.CharField(max_length=10, validators=[validators.MinLengthValidator(limit_value=0)],
-                               null=True, blank=True, verbose_name="悦谱比对时间戳")
+                               null=True, blank=True, help_text='系统生成',verbose_name="悦谱比对时间戳")
     vs_time_g = models.CharField(max_length=10, validators=[validators.MinLengthValidator(limit_value=0)],
-                                  null=True, blank=True, verbose_name="G比对时间戳")
+                                  null=True, blank=True,help_text='系统生成', verbose_name="G比对时间戳")
 
     objects = models.Manager()  # 默认的管理器
     published = JobManager()  # 自定义管理器
 
-    # tags=TaggableManager()
-    # 声明这个manager也是基于我们自定义的模型类
-    tags = TaggableManager(through=TaggedWhatever)
+
 
 
     #以下是梅炽元的接口测试需求
@@ -119,7 +138,7 @@ class Job(models.Model):
                                      validators=[validators.MaxValueValidator(100000), validators.MinValueValidator(0)],help_text="阻抗线数量(包括槽孔,镭射孔等)",verbose_name='阻抗线数量')
 
     customerCode=models.CharField(null=True,blank=True,max_length=50, validators=[validators.MinLengthValidator(limit_value=0)],help_text="客户(代码)",verbose_name='客户(代码)')
-    hasPGlayer=models.CharField(max_length=10, choices=(('yes', '是'), ('no', '否'),('none', 'None')), default='none',help_text="有PG层",verbose_name="有PG层?")
+    hasPGlayer=models.CharField(max_length=10, choices=(('yes', '是'), ('no', '否'),('none', 'None')), default='none',help_text="有PG层?",verbose_name="有PG层?")
     hasSMlayer=models.CharField(max_length=10, choices=(('yes', '是'), ('no', '否'),('none', 'None')), default='none',help_text="有solderMasker层吗?",verbose_name="有防焊层?")
     solderMaxWindowNum4singleSide=models.IntegerField(null=True,blank=True,
                                      validators=[validators.MaxValueValidator(100000000), validators.MinValueValidator(0)],help_text="单面最多防焊开窗数量",verbose_name='单面最多防焊开窗数量')
