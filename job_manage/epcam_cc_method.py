d6d3a8f8e78fcad4bffa0ae0fdf74d7aaa798229
naivete13sky <chen320821@163.com>
2022-09-14 19:20:08 +0800

    20220914 7 修改了epcam_cc_method.py，增加了给pytest调用的方法。

--- job_manage/epcam_cc_method.py
+++ job_manage/epcam_cc_method.py
@@ -219,6 +219,111 @@ class EpGerberToODB:
         # time.sleep(20)
         # job_operation.delete_job(job)#delete会把odb文件夹删除的
 
+    def traverse_gerber_pytest(self,job, step, file_path, index,job_id):
+        # print("*"*30,job_id)
+        job_current = models.Job.objects.get(id=job_id)
+        layer_all = models.Layer.objects.filter(job=job_current)
+        print(layer_all)
+
+        """转换Gerber文件
+        :param     job(str):job名
+        :param     step(str):step名
+        :param     file_path(str):文件路径
+        :param     index(int):序号
+        :returns   :None
+        :raises    error:
+        """
+        for root, dirs, files in os.walk(file_path):
+            epcam_api.file_translate_init(job)
+            for file in files:
+                if self.is_chinese(file):
+                    os.rename(file_path + r'/' + file, file_path + r'/''unknow' + str(index))
+                    file = 'unknow' + str(index)
+                    index = index + 1
+                os.rename(file_path + r'/' + file,file_path + r'/' + file.replace(' ','-').replace('(','-').replace(')','-'))
+                print("file name:",file.replace(' ','-').replace('(','-').replace(')','-'))
+                ret = epcam_api.file_identify(os.path.join(root, file.replace(' ','-').replace('(','-').replace(')','-')))
+                print("ret:",ret)
+                data = json.loads(ret)
+                file_format = data['paras']['format']
+                file_name = data['paras']['name']
+                file_param = data['paras']['parameters']
+                # file_param = {'Coordinates':'Absolute',
+                #               'Decimal_numbers':True,
+                #               'Number_format_integer':2,
+                #               'Number_format_decimal':4,
+                #               'data_type':'Ascii',
+                #               'file_size':1391.0,
+                #               'format':'Excellon2',
+                #               'separator_char':'nl',
+                #               'text_line_width':0.0,
+                #               'tool_units':'Inch',
+                #               'units':'Inch',
+                #               'zeroes_omitted':'Leading'}
+
+
+
+                if file_format == 'Excellon2':
+                    # re = epcam_api.file_translate(os.path.join(root, file), job, step, file, file_param, '', '', '',[])
+                    print('file:',file)
+                    print('?=',file.replace(' ','-').replace('(','-').replace(')','-'))
+                    # for each in models.Layer.objects.filter(job=job_current):
+                    #     print(each.layer,each.layer_org)
+                    print(file,'=?',models.Layer.objects.get(job=job_current,layer=file.replace(' ','-').replace('(','-').replace(')','-')))
+                    print('原来：',file_param)
+                    try:
+                        layer_e2= models.Layer.objects.get(job=job_current,layer=file.replace(' ','-').replace('(','-').replace(')','-'))
+                        print(layer_e2.layer,layer_e2.status)
+                        if layer_e2.status=='published':
+                            pass
+                            file_param['units']=layer_e2.units_ep
+                            file_param['zeroes_omitted'] = layer_e2.zeroes_omitted_ep
+                            file_param['Number_format_integer'] = int(layer_e2.number_format_A_ep)
+                            file_param['Number_format_decimal'] = int(layer_e2.number_format_B_ep)
+                            file_param['tool_units'] = layer_e2.tool_units_ep
+                        print('现在：',file_param)
+                        re = epcam_api.file_translate(os.path.join(root, file.replace(' ','-').replace('(','-').replace(')','-')), job, step, file_name, file_param, '', '', '',[])
+                    except:
+                        print("except:"*5)
+                        re = epcam_api.file_translate(os.path.join(root, file.replace(' ','-').replace('(','-').replace(')','-')), job, step, file_name, file_param, '', '', '',[])
+
+                if file_format == 'Gerber274x' or file_format == 'DXF':
+                    print(file)
+                    re = epcam_api.file_translate(os.path.join(root, file.replace(' ','-').replace('(','-').replace(')','-')), job, step, file_name, file_param, '', '', '',[])  # translate
+
+    def ep_gerber_to_odb_pytest(self,job, step, file_path, out_path,job_id):
+        """Gerber转ODB
+        :param     job(str):job名
+        :param     step(str):step名
+        :param     file_path(str):gerber文件夹路径
+        :param     out_path:输出odb路径
+        :returns   :None
+        :raises    error:
+        """
+        # epcam.init()
+        new_job_path = os.path.join(out_path, job)  # job若存在则删除
+        if os.path.exists(new_job_path):
+            shutil.rmtree(new_job_path)
+        epcam_api.create_job(out_path, job)
+
+
+        # time.sleep(20)
+
+
+
+        job_operation.open_job(out_path, job)
+        job_operation.create_step(job, step)
+        job_operation.save_job(job)
+        index = 1
+        print("*"*100,job, step, file_path, index,job_id)
+        self.traverse_gerber_pytest(job, step, file_path, index,job_id)
+        job_operation.save_job(job)
+        all_layer = layer_info.get_all_layer_name(job)  # 获得料号下所有layer
+        gl.all_layer=all_layer
+        #清空内存，防止同名料号出问题
+        epcam_api.close_job(job)
+        # time.sleep(20)
+        # job_operation.delete_job(job)#delete会把odb文件夹删除的
 
 if __name__ == "__main__":
     pass
